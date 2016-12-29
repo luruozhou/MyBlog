967805a21d1c2cfa42dd1b20002a5bf5542e66eb
MsCtrl0 <940356316@qq.com>
2016-12-29 10:47:47 +0800

    创建：文章详情页

--- views/page/article-detail.tpl
+++ views/page/article-detail.tpl
@@ -22,9 +22,23 @@
 
 <body>
     {% include "../widget/layout/header/header.tpl"%}
-   {{JSON.stringify(articleRecord)}}
+   <!--{{JSON.stringify(articleRecord)}}-->
     {#下边不要写任何代码(注释也不要删)，否则会发生无法挽回的错误#}
     <!--SCRIPT_PLACEHOLDER-->
+    <div class="article-type">
+        <a href="#">技术</a>
+        <a href="#">nodeJS</a>
+    </div>
+    <div class="article-title">
+        剿灭 jQuery 的第一步（IE8+）
+    </div>
+    <div class="article-time">
+        2016.12.28
+    </div>
+    <div class="article-main">
+        
+    </div>
+    <div class="back-top"></div>
     <script type="text/javascript">
         require('../static/article-detail/article-detail')
     </script>
--- views/static/article-detail/article-detail.js
+++ views/static/article-detail/article-detail.js
@@ -3,9 +3,37 @@ var EventUtil=require('../libs/js/util').EventUtil;
 $(function () {
     //获取页面可用高度
 	var height = document.documentElement.clientHeight;
-    var doc=document.querySelector("body");
-    console.log("EventUtil");
-    EventUtil.addHandler(doc,"mousewheel",function(){
-        console.log(event.wheelDelta );
+    
+    // 获取body
+    var body=document.querySelector("body");
+    //获取header
+    // var topbar=$(".topbar");
+    // var scrollTop=document.body.scrollTop;
+    //     console.log( document.body.scrollTop)
+
+    //     // 下滚
+    //     if(scrollTop>0){
+    //         topbar.animate({"opciaty":"0"},1000);
+    //     }
+    //     if(event.wheelDelta>0){
+    //         topbar.animate({"top":"0px"},1000);
+    //     }
+    //     console.log(event.wheelDelta);
+    EventUtil.addHandler(body,"mousewheel",function(){
+        var scrollTop=document.body.scrollTop;
+        console.log(scrollTop);
+        console.log("dd");
+        if(scrollTop>height){
+            $(".back-top").css({"transform": 'translate(0px,10px)',"-ms-transform":" translate(0px,10px)","-webkit-transform": "translate(0px,10px)", " -o-transform": "translate(0px,10px)", "-moz-transform":" translate(0px,10px)"});
+        }else{
+            $(".back-top").css({"transform": 'translate(0px,40px)',"-ms-transform":" translate(0px,40px)","-webkit-transform": "translate(0px,40px)", " -o-transform": "translate(0px,40px)", "-moz-transform":" translate(0px,40px)"}); 
+        }
+        
     })
+    $(".back-top").click(function(){
+        document.body.scrollTop=0;
+    })
+
+    
+
 })
