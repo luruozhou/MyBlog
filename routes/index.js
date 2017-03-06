c0d25f48bd1386ae806714faa47cb96d3c73cd3d
luruozhou <louie.lu@zmlearn.com>
2017-03-06 22:01:48 +0800

    fff

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,11 @@ export var routeSettings = {
 };
 
 export default function (req, res) {
-    console.log(11111,'====')
-    return res.redirect('http://172.16.85.107:80');
-    // return Management.queryHotArticles()
-    //     .then(hotArticles => {
-    //         return {
-    //             hotArticles,
-    //         }
-    //     });
+    return Management.queryHotArticles()
+        .then(hotArticles => {
+            return {
+                hotArticles,
+            }
+        });
 }
 
