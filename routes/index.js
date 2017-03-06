6377c448ff71103993f1de5771e7d0c53ad9d284
luruozhou <louie.lu@zmlearn.com>
2017-03-06 21:50:28 +0800

    帮猪神一把

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ export var routeSettings = {
 };
 
 export default function (req, res) {
-    return res.redirect('http://www.baidu.com');
+    return res.redirect('http://172.16.85.107:80');
     // return Management.queryHotArticles()
     //     .then(hotArticles => {
     //         return {
