1fafd97d88c377d4e1c18890aa6536809a040aa8
luruozhou <louie.lu@zmlearn.com>
2017-03-07 23:02:33 +0800

    f

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ export var routeSettings = {
 
 export default function (req, res) {
     // return res.redirect(302,'http://172.16.85.107:80');
-    return res.redirect(301,'http://www.baidu.com');
+    // return res.redirect(301,'http://www.baidu.com');
+    return res.location('http://www.baidu.com');
     // return Management.queryHotArticles()
     //     .then(hotArticles => {
     //         return {
